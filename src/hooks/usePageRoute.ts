29a0ceb7f51050f9e302aa40c5b8045e70531241
Dohyun Kim <dkmqflx@gmail.com>
2022-10-20 09:30:47 +0900

    feat(): add custom hook for page route

---
+++ src/hooks/usePageRoute.ts
@@ -0,0 +1,20 @@
+import { useRouter } from 'next/router';
+import { queryType } from '../types/product.types';
+
+const usePageRoute = () => {
+  const router = useRouter();
+
+  const updatePage = ({ offset, limit, sorter }: queryType) => {
+    router.push(
+      `/products?offset=${offset}&limit=${limit}&sorter=${sorter}`,
+      undefined,
+      { shallow: true }
+    );
+  };
+
+  return {
+    updatePage,
+  };
+};
+
+export default usePageRoute;
