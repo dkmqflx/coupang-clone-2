1bc5adaa669e9bcf6d7693807b9fb3541a69438d
Dohyun Kim <dkmqflx@gmail.com>
2022-11-15 20:47:35 +0900

    feat(): fetch data by server side rendering

--- pages/checkout/[id].tsx
+++ pages/checkout/[id].tsx
@@ -1,8 +1,18 @@
+import { GetServerSideProps } from 'next';
+import { AuthService, CheckoutService } from '../../src/services';
 import Order from '../../src/components/Order';
 import PaymentInfo from '../../src/components/PaymentInfo';
+import { orderSheetType } from '../../src/types/order';
+import { EMAIL, PASSWORD } from '../../src/constants/login';
 import styled from '@emotion/styled';
+import { parseCookie } from '../../src/utils';
 
-export default function CheckoutPage() {
+export default function CheckoutPage({
+  orderSheet,
+}: {
+  orderSheet: orderSheetType;
+}) {
+  console.log(orderSheet);
   return (
     <Wrapper>
       <Order></Order>
@@ -10,6 +20,21 @@ export default function CheckoutPage() {
     </Wrapper>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  let token = parseCookie(context.req.headers.cookie, 'accessToken');
+
+  if (!token) {
+    const { access } = await AuthService.login(EMAIL, PASSWORD);
+    context.res.setHeader('Set-Cookie', [`accessToken=${access}`]);
+    token = access;
+  }
+
+  const { id } = context.query;
+  const data = await CheckoutService.getOrderSheet(id as string, token);
+
+  return { props: { orderSheet: data } };
+};
 
 const Wrapper = styled.div`
   width: 1020px;
