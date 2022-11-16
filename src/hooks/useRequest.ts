d4c06ccdfb85daeb41ab49bd467608a997b9cc1d
Dohyun Kim <dkmqflx@gmail.com>
2022-11-16 20:00:11 +0900

    feat(): add useRequest hook using react query

--- src/hooks/useRequest.ts
+++ src/hooks/useRequest.ts
@@ -1,1 +1,14 @@
-export const useRequest = () => {};
+import {
+  QueryKey,
+  QueryFunction,
+  UseQueryOptions,
+  useQuery,
+} from 'react-query';
+
+export const useRequest = (
+  key: QueryKey,
+  request: QueryFunction,
+  option?: UseQueryOptions
+): any => {
+  return useQuery(key, request, { ...option });
+};
